222679334ae70d21cf9c609363d15de8974902f4
15317660613 <624859128@qq.com>
2021-01-20 17:02:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <span>213</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/page/home/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="contain">
+    <span>123</span>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods:{
+
+  },
+  mounted(){
+    
+  },
+  watch:{
+
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
